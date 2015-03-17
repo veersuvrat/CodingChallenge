516b2a3221701909c10edeff0c64713364e654aa
veersuvrat <veer2812@gmail.com>
2015-03-17 14:22:10 -0700

    cleaner q1
    Comments added to q1.js

--- q1.js
+++ q1.js
@@ -1,7 +1,20 @@
+// initially set up all frequencies = 0
+var frequencies = [];
+for (var i = 0; i < 27; i++) {
+  frequencies[i] = 0;
+}
+d3.select("body").selectAll("div").data(frequencies).enter().append("div").attr("class", "bar").style("height", displayHeight);
+
+
+
+
 function SearchUsingInput(){
   var searchBox = document.getElementById("SearchForm");
+  
   console.log("Just testing things out.");
+  // Checking to see if the Search input is processed.
   console.log(searchBox.elements[0].value);
+  
   xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", "https://api.angel.co/1/search?query=" + searchBox.elements[0].value + "&type=User", false);
   xmlHttp.send(null);
@@ -9,25 +22,20 @@ function SearchUsingInput(){
   var jsonResponse = JSON.parse(xmlHttp.responseText);
   var NumberOfUsers = jsonResponse.length;
 
+  // if a search query returns less <= 5 results, process all. Else process the first 5.
   var IterationLimit = Math.min(5, NumberOfUsers);
 
   for (var i = 0; i < IterationLimit; i++) {
     parseUserName(jsonResponse[i].name);
   }
-  //d3.select("body").selectAll("div").data(newFreq).style("height", displayHeight);
-  //console.log(document.getElementsByClassName("bar"));
-  //document.getElementsByClassName("bar")[0].style.height = 100 + "px";
-  //console.log(d3.selectAll(".bar"));
+  
 
   d3.selectAll(".bar").data(frequencies).style("height", displayHeight);
 
 }
 
-var frequencies = [];
-for (var i = 0; i < 27; i++) {
-  frequencies[i] = 0;
-}
 
+// parse each username as it comes in from the SearchUsingInput() function.
 
 function parseUserName(UserName){
 
@@ -36,6 +44,8 @@ function parseUserName(UserName){
   for (var i = 0; i < LowerCase.length; i++) {
     var unicode = LowerCase.charCodeAt(i);
     var indexToIncrement = unicode - 97;
+
+    // checking to see if the character is an alphabet or 'other'.
     if ((indexToIncrement>=0) && (indexToIncrement<=25)) {
       frequencies[indexToIncrement] = frequencies[indexToIncrement] + 1;
     }else{
@@ -44,8 +54,9 @@ function parseUserName(UserName){
   }
 }
 
-d3.select("body").selectAll("div").data(frequencies).enter().append("div").attr("class", "bar").style("height", displayHeight);
 
+
+// Make the height of the bars more visible. The frequencies themselves are very small for only 5 usernames
 
 function displayHeight(originalHeight){
   return originalHeight*30 + "px";
